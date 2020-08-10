959465eb5e5bf605efd8c593bfb8a8d7b6e5dcd1
subham-proj <3133ss32@gmail.com>
2020-08-10 16:48:14 +0530

    d6.1

--- staticpages/urls.py
+++ staticpages/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+        path('about/', views.about, name='about'),
         path('contact/', views.contact, name='contact'),
-        path('privacy/', views.privacy, name='privacy'), 
+        path('privacy/', views.privacy, name='privacy'),
+        path('contribute/', views.contribute, name='contribute'),
 
 ]
--- staticpages/views.py
+++ staticpages/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def about(request):
+    return render(request, 'staticpages/about.html')
+
+
 def contact(request):
     return render(request, 'staticpages/contact.html')
 
 
 def privacy(request):
     return render(request, 'staticpages/privacy.html')
+
+def contribute(request):
+    return render(request, 'staticpages/contribute.html')
 
